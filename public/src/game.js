01ae2a3ce458ebf86a3c7f051ab3ececc5a4c017
Rien3844 <102456478+Rien3844@users.noreply.github.com>
2024-10-15 09:41:06 +0900

    token
    #5 만든 토큰 game.js에서 받아오기

--- public/src/game.js
+++ public/src/game.js
@@ -248,7 +248,7 @@ Promise.all([
   ...monsterImages.map((img) => new Promise((resolve) => (img.onload = resolve))),
 ]).then(() => {
   /* 서버 접속 코드 (여기도 완성해주세요!) */
-  let somewhere;
+  let somewhere = localStorage.getItem('Authorization');
   serverSocket = io('서버주소', {
     auth: {
       token: somewhere, // 토큰이 저장된 어딘가에서 가져와야 합니다!
